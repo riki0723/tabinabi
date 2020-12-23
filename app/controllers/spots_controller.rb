d6d3adc1e93b840c9d799e2a6314acd3952e6de0
riki0723 <69281517+riki0723@users.noreply.github.com>
2020-12-23 20:19:11 +0900

    詳細画面の表示

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -7,6 +7,10 @@ class SpotsController < ApplicationController
     Spot.create(spot_params)
   end
 
+  def show
+    @spot = Spot.find(params[:id])
+  end
+
   private
   def spot_params
     params.require(:spot).permit(:name, :text, :image).merge(user_id: current_user.id)
