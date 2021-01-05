214749831f70c3fc1ce44fb3c72c3ade9bd4fda3
riki0723 <69281517+riki0723@users.noreply.github.com>
2021-01-06 00:05:27 +0900

    複数挑戦

--- app/models/spots_tag.rb
+++ app/models/spots_tag.rb
@@ -6,15 +6,20 @@ class SpotsTag
   with_options presence: true do
     validates :title, :text,:image
     validates :area_id, numericality: { other_than: 1 }
-    validates :name 
+    validates :name
   end
 
   def save
     spot = Spot.create(title: title, text: text, image:image, area_id:area_id, user_id:user_id)
+
+    # tag_list = Tag.name.split(nil)
+    # tag = Tag.where(name: tag_list).first_or_initialize
     tag = Tag.where(name: name).first_or_initialize
     tag.save
 
     SpotTagRelation.create(spot_id: spot.id, tag_id: tag.id)
   end
+
+  
   
 end
