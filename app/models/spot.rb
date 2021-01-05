9f75545886903582808ec11d60d9306f77f1deec
riki0723 <69281517+riki0723@users.noreply.github.com>
2021-01-06 00:46:36 +0900

    messagesテーブルの作成

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -5,6 +5,7 @@ class Spot < ApplicationRecord
   has_one_attached :image
   has_many :spot_tag_relations, dependent: :destroy
   has_many :tags, through: :spot_tag_relations 
+  has_many :messages
   
   # validates :title, :text,:image, presence: true
   # validates :area_id, numericality: { other_than: 1 } 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_103956) do
+ActiveRecord::Schema.define(version: 2021_01_05_154145) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 2020_12_30_103956) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.bigint "spot_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["spot_id"], name: "index_messages_on_spot_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "spot_tag_relations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -71,6 +81,8 @@ ActiveRecord::Schema.define(version: 2020_12_30_103956) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "messages", "spots"
+  add_foreign_key "messages", "users"
   add_foreign_key "spot_tag_relations", "spots"
   add_foreign_key "spot_tag_relations", "tags"
 end
