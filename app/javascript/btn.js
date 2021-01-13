d7bc5524057db5088e8ffd8b7aea5e64510cf055
riki0723 <69281517+riki0723@users.noreply.github.com>
2021-01-14 03:13:47 +0900

    タグボタンにscriptを実装

--- app/javascript/btn.js
+++ app/javascript/btn.js
@@ -16,3 +16,22 @@ function cardBtn(){
   
 }
 window.addEventListener('load', cardBtn)
+
+function tagBtn(){
+
+  const CardButton = document.querySelectorAll(".card_tag_list")
+
+  CardButton.forEach(function(list) {
+    list.addEventListener('mouseover', function(){
+        list.setAttribute("style", "background-color: gold;")
+      })
+  })
+
+  CardButton.forEach(function(list) {
+    list.addEventListener('mouseout', function(){
+        list.removeAttribute("style", "background-color: gold;")
+      })
+  })
+  
+}
+window.addEventListener('load', tagBtn)
