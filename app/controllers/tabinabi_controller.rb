60b777392119ff938ec64bd653ab5b5a9c92e709
riki0723 <69281517+riki0723@users.noreply.github.com>
2021-01-15 06:28:09 +0900

    トップページのページネーション実装

--- app/controllers/tabinabi_controller.rb
+++ app/controllers/tabinabi_controller.rb
@@ -1,7 +1,7 @@
 class TabinabiController < ApplicationController
 
   def index
-    @spots = Spot.all
+    @spots = Spot.all.page(params[:page]).per(10)
     @tag_list = Tag.all
     # @tag = Tag.find(params[:tag_id])
   end
