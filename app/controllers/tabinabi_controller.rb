607268dcdc7a4e6261658efa14c0b50dfe71a239
riki0723 <69281517+riki0723@users.noreply.github.com>
2021-01-15 06:34:54 +0900

    マイページのページネーション実装

--- app/controllers/tabinabi_controller.rb
+++ app/controllers/tabinabi_controller.rb
@@ -1,7 +1,7 @@
 class TabinabiController < ApplicationController
 
   def index
-    @spots = Spot.all.page(params[:page]).per(10)
+    @spots = Spot.all.page(params[:page]).per(10).order("created_at DESC")
     @tag_list = Tag.all
     # @tag = Tag.find(params[:tag_id])
   end
