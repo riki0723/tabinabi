6670ba7c59dd35eda85a9e88fa00f83384d478a2
riki0723 <69281517+riki0723@users.noreply.github.com>
2021-01-17 02:25:43 +0900

    プレビュー実装

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,3 +1,29 @@
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
 document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('image-list');
+
+  const createImageHTML = (blob) => {
+
+   const imageElement = document.createElement('div');
+
+
+   const blobImage = document.createElement('img');
+   blobImage.setAttribute('src', blob);
+   // 生成したHTMLの要素をブラウザに表示させる
+   imageElement.appendChild(blobImage);
+   ImageList.appendChild(imageElement);
+ };
+
+  document.getElementById('spot_image').addEventListener('change', function(e){
+    // 画像が表示されている場合のみ、すでに存在している画像を削除する
+    const imageContent = document.querySelector('img');
+    if (imageContent){
+      imageContent.remove();
+    }
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    createImageHTML(blob);
+  });
 });
+}
